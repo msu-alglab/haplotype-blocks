f7c09dcc63c46886d717efdc728d71fc08aec579
Lucy <lgw2@uw.edu>
2019-12-09 08:11:22 -0700

    alert if no snps

--- create_scatter.py
+++ create_scatter.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-
 f = open("output.k100.50.txt")
 x = []
 y = []
@@ -14,14 +13,11 @@ for line in f.readlines():
             y.append(len(line.split()))
         else:
             y.append(0)
+            print("no SNPS")
         counter += 1
 
     counter = counter % 2
-indices = [i for (i, val) in enumerate(y) if val != 0]
-x = [v for (i, v) in enumerate(x) if i in indices]
-y = [v for (i, v) in enumerate(y) if i in indices]
 
-print(indices)
 plt.scatter(x,y, c="red", alpha=0.1)
 
 f = open("output.k1000.20.txt")
@@ -36,13 +32,11 @@ for line in f.readlines():
         if line != "No SNPS\n":
             y.append(len(line.split()))
         else:
+            print("no SNPS")
             y.append(0)
         counter += 1
     counter = counter % 2
 
-indices = [i for (i, val) in enumerate(y) if val != 0]
-x = [v for (i, v) in enumerate(x) if i in indices]
-y = [v for (i, v) in enumerate(y) if i in indices]
 plt.scatter(x,y, c="blue", alpha=0.3)
 
 plt.xlabel("Number of paths")
