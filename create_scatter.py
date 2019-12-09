afdcfa53f18b16d4abde678f8f341e229b310f46
Lucy <lgw2@uw.edu>
2019-12-08 19:06:30 -0700

    udpate scatter

--- create_scatter.py
+++ create_scatter.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-f = open("output.txt")
+f = open("output.k1000.20.txt")
 x = []
 y = []
 counter = 0
@@ -9,9 +9,26 @@ for line in f.readlines():
         x.append(len(line.split()))
         counter += 1
     else:
-        y.append(len(line.split()))
+        if line != "No SNPS":
+            y.append(len(line.split()))
         counter += 1
     counter = counter % 2
 
-plt.scatter(x,y)
+plt.scatter(x,y, c="blue")
+
+f = open("output.k100.50.txt")
+x = []
+y = []
+counter = 0
+for line in f.readlines():
+    if counter == 0:
+        x.append(len(line.split()))
+        counter += 1
+    else:
+        if line != "No SNPS":
+            y.append(len(line.split()))
+        counter += 1
+    counter = counter % 2
+
+plt.scatter(x,y, c="red")
 plt.savefig("scatterplot.png")
