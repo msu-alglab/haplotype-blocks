4aa229857afe84c82f99b6882405768a9c617ac8
Lucy <lgw2@uw.edu>
2020-01-08 11:39:02 -0700

    run experiments

--- wildcard/find_blocks.py
+++ wildcard/find_blocks.py
@@ -1,3 +1,8 @@
+import random
+import sys
+sys.setrecursionlimit(11000)
+
+
 def check_input(seqs):
     """Check that input is right format"""
     n = len(seqs[0])
@@ -26,14 +31,12 @@ def find_blocks(seqs):
     n = len(seqs[0])
     k = len(seqs)
 
-    print("Input n={}, k={}".format(n, k))
-    print()
     sets = get_sets(seqs, n, k)
 
     for col in range(n):
-        print("Finding blocks at index {}".format(col + 1))
+        if col % 1000 == 1:
+            print("Finding blocks at index {}".format(col + 1))
         DFS(col, set(range(k)))
-        print()
 
 
 def get_sets(seqs, n, k):
@@ -70,40 +73,43 @@ def DFS(i, rows):
     for b in [0, 1]:
         pathsets.append(sets[i][b])
         pathOK = True
-        print("Checking the {} branch".format(b))
-        print("Pathsets is currently", pathsets)
+        # print("Checking the {} branch".format(b))
+        # print("Pathsets is currently", pathsets)
         for s in pathsets:
             if not bool(rows.intersection(s)):
                 pathOK = False
                 break
         rows_b = rows.intersection(sets[i][b].union(sets[i]["*"]))
-        print("Rows supporting this branch: {}".format(rows_b))
+        # print("Rows supporting this branch: {}".format(rows_b))
         for s in pathsets:
             if not bool(rows_b.intersection(s)):
                 pathOK = False
                 break
-        print("Is this path okay?", pathOK)
+        # print("Is this path okay?", pathOK)
         right0 = rows_b.intersection(sets[col + 1][0])
         right1 = rows_b.intersection(sets[col + 1][1])
         if pathOK and bool(right0) and bool(right1) and len(rows_b) > 1:
             branch_count += 1
             if i == 0 or DFS(i - 1, rows_b) != 1:
                 rows_to_print = [x + 1 for x in rows_b]
-                print("Found block: K={}, i={}, j={}".format(
+                g.write("K={}, i={}, j={}\n".format(
                     rows_to_print, i + 1, col + 1))
         pathsets.pop()
     return branch_count
 
 
-def read_data(filename):
+def create_data(filename, k, n, wc_prop):
     """Read in binary matrix file."""
+    print("Reading in file...\n")
     f = open(filename)
     seqs = []
-    n = 100
-    for line in f.readlines()[:50]:
+    for line in f.readlines()[:k]:
         seq = []
         for character in line[:n]:
-            seq.append(int(character))
+            if random.random() > wc_prop:
+                seq.append(int(character))
+            else:
+                seq.append('*')
         seqs.append(seq)
     return seqs
 
@@ -115,6 +121,17 @@ if __name__ == "__main__":
               [0, 0, 1],
               [0, 0, '*']]
     """
-    blocks = read_data("/Users/lucywilliams/vcfs/output.txt")
+    random.seed(1)
+    global g
 
-    find_blocks(blocks)
+    for wc_prop in [0.0, 0.05]:
+        output_name = "blocks" + str(wc_prop) + ".txt"
+        g = open(output_name, "w")
+        blocks = create_data(
+            "/home/lucy/vcfs/output.txt",
+            1000,
+            10000,
+            wc_prop
+        )
+        find_blocks(blocks)
+        g.close()
