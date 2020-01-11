ad2963f93234fed0249810a0f9b2c5b64d85e044
Lucy <lgw2@uw.edu>
2020-01-10 18:02:28 -0700

    Update with new notation

--- wildcard/find_blocks.py
+++ wildcard/find_blocks.py
@@ -13,7 +13,7 @@ def check_input(seqs):
         assert len(seq) == n, "Not all sequences same length"
 
 
-def find_blocks(seqs):
+def find_blocks(seqs, outputfile):
     """Find all maximal perfect wildcard haplotype blocks in a set of
     sequences"""
     check_input(seqs)
@@ -26,7 +26,8 @@ def find_blocks(seqs):
     global n
     global k
     global sets
-    global nonwc
+    global cons_cnt
+
     n = len(seqs[0])
     k = len(seqs)
 
@@ -35,15 +36,17 @@ def find_blocks(seqs):
 
     sets = get_sets(seqs, n, k)
 
+    global g
+    g = open(outputfile, "w")
+
     # for col in range(n):
-    for col in [2]:
-        nonwc = [0]*n
-        """
+    for col in range(n):
+        cons_cnt = [0]*n
         if col % 1000 == 1:
             print("Finding blocks at index {}".format(col + 1))
-        """
-        print("Finding blocks at index {}".format(col + 1))
         DFS(col, set(range(k)))
+
+    g.close()
 
 
 def get_sets(seqs, n, k):
@@ -69,6 +72,7 @@ def get_sets(seqs, n, k):
     sets[n] = dict()
     for b in [0, 1]:
         sets[n][b] = list(range(k))
+    sets[n]['*'] = list(range(k))
 
     return sets
 
@@ -76,69 +80,73 @@ def get_sets(seqs, n, k):
 def DFS(i, rows):
     """Explore one layer deeper in the binary trie for MPWHBs at a certain
     i."""
-    print("Call to DFS with i={} and rows={}".format(i, rows))
+    # print("Call to DFS with i={} and rows={}".format(i, rows))
     branch_count = 0
     for b in [0, 1]:
-        print("Explore b={} branch".format(b))
-        rm_rows = rows.intersection(sets[i][abs(1 - b)])
-        retain_rows = rows.difference(rm_rows)
+        # print("Explore b={} branch".format(b))
+        kp = rows.intersection(sets[i][b].union(sets[i]['*']))
+        rm = rows.intersection(sets[i][abs(1-b)])
 
-        print("Rows to remove: {}".format(rm_rows))
-        print("Rows to retain: {}".format(retain_rows))
+        # print("Rows to remove: {}".format(rm))
+        # print("Rows to retain: {}".format(kp))
 
         # update position i of nonwc
-        for r in retain_rows:
+        for r in kp:
             if s[r][i] == b:
-                nonwc[i] += 1
-        print("nonwc is {}".format(nonwc))
-        pathOK = nonwc[i] > 0
+                cons_cnt[i] += 1
+        # print("cons_cnt is {}".format(cons_cnt))
+        consOK = cons_cnt[i] > 0
 
-        print("Processing nonwc for rm rows")
+        # print("Processing nonwc for rm rows")
         # for each row that was removed, for each column other than the current
         # column, subtract 1 if the row at that column was a non-wildcard
         # character
-        for r in rm_rows:
-            print("r={}".format(r))
+        for r in rm:
+            # print("r={}".format(r))
             for c in range(i + 1, col + 1):
-                print("c={}".format(c))
-                print("s[{}][{}]={}".format(r, c, s[r][c]))
+                # print("c={}".format(c))
+                # print("s[{}][{}]={}".format(r, c, s[r][c]))
                 if s[r][c] != '*':
-                    nonwc[c] -= 1
-                    if nonwc[c] <= 0:
-                        pathOK = False
-        print("nowc is {}".format(nonwc))
+                    cons_cnt[c] -= 1
+                    if cons_cnt[c] <= 0:
+                        consOK = False
+        # print("cons_cnt is {}".format(cons_cnt))
 
-        print("Is this path okay?", pathOK)
-        right0 = retain_rows.intersection(sets[col + 1][0])
-        right1 = retain_rows.intersection(sets[col + 1][1])
-        print("Right-maximal: {}. Left-maximal: {}".format(right0, right1))
-        if pathOK and bool(right0) and bool(right1):
-            # BUG: we are over-counting
+        # print("Is this path okay?", consOK)
+        right0 = bool(kp.intersection(sets[col + 1][0]))
+        right1 = bool(kp.intersection(sets[col + 1][1]))
+        rightwc = kp.issubset(sets[col + 1]['*'])
+        # print("Right0={}, right1={}, rightallwc={}".format(
+        #    right0,
+        #    right1,
+        #    rightwc))
+        r_max = col == n or right0 and right1 or rightwc
+        if consOK and r_max:
             branch_count += 1
-            if len(retain_rows) > 1:
-                if i == 0 or DFS(i - 1, retain_rows) == 1:
-                    rows_to_print = [x + 1 for x in retain_rows]
-                    print("Found block K={}, i={}, j={}".format(
-                        rows_to_print, i + 1, col + 1))
-                    """
+            # print("Branch count increment, so bc={}".format(branch_count))
+            if len(kp) > 1:
+                if i == 0 or DFS(i - 1, kp) != 1:
+                    rows_to_print = [x + 1 for x in kp]
+                    # print("Found block K={}, i={}, j={}".format(
+                    #     rows_to_print, i + 1, col + 1))
                     g.write("K={}, i={}, j={}\n".format(
                         rows_to_print, i + 1, col + 1))
-                    """
             else:
-                print("Stopped pursuing because |remain_rows| == 1")
+                # print("Stopped pursuing because |remain_rows| == 1")
+                pass
 
-        for r in retain_rows:
+        for r in kp:
             if s[r][i] == b:
-                nonwc[i] -= 1
+                cons_cnt[i] -= 1
 
-        for r in rm_rows:
+        for r in rm:
             for c in range(i + 1, col + 1):
                 if s[r][c] != '*':
-                    nonwc[c] += 1
-        print("going back up the tree. nonwc has been returned to {}".format(
-            nonwc))
+                    cons_cnt[c] += 1
+        # print("going back up the tree. nonwc has been returned to {}".format(
+        #     cons_cnt))
 
-    print("Call to DFS with i={} and rows={} OVER".format(i, rows))
+    # print("Call to DFS with i={} and rows={} OVER".format(i, rows))
     return branch_count
 
 
@@ -159,25 +167,15 @@ def create_data(filename, k, n, wc_prop):
 
 
 if __name__ == "__main__":
-    blocks = [[1, 0, "*"],
-              [1, 0, "*"],
-              [0, 0, 0],
-              [0, 0, 1]]
-    find_blocks(blocks)
 
-    """
     random.seed(1)
-    global g
 
-    for wc_prop in [0.0, 0.05]:
+    for wc_prop in [0.05]:
         output_name = "blocks" + str(wc_prop) + ".txt"
-        g = open(output_name, "w")
         blocks = create_data(
             "/home/lucy/vcfs/output.txt",
-            1000,
+            100,
             10000,
             wc_prop
         )
-        find_blocks(blocks)
-        g.close()
-    """
+        find_blocks(blocks, output_name)
