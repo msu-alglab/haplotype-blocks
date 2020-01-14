9568903b3a4a932577988bee29416046e5b999bc
Lucy <lgw2@uw.edu>
2020-01-14 11:48:45 -0700

    Pull out stats computation to function and add k-25

--- pangenome/get_stats.py
+++ pangenome/get_stats.py
@@ -1,61 +1,35 @@
-import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.rcParams.update({'font.size': '14'})
-
-f = open("output.k100.50.txt")
-x = []
-y = []
-counter = 0
-for line in f.readlines():
-    if counter == 0:
-        x.append(len(line.split()))
-        counter += 1
-    else:
-        if line != "No SNPS\n":
-            y.append(len(line.split()))
+def compute_stats(filename):
+    """Given a pangenome hap output file, compute avg paths and avg snps"""
+    f = open(filename)
+    x = []
+    y = []
+    counter = 0
+    for line in f.readlines():
+        if counter == 0:
+            x.append(len(line.split()))
+            counter += 1
         else:
-            y.append(0)
-            print("no SNPS")
-        counter += 1
+            if line != "No SNPS\n":
+                y.append(len(line.split()))
+            else:
+                print("no SNPS")
+                y.append(0)
+            counter += 1
+        counter = counter % 2
+    k = filename.split(".")[1][1:]
+    print("For k= {}".format(k))
+    total_paths = sum(x)
+    avg_paths = total_paths/len(x)
+    print("Avg paths:", avg_paths)
+    total_snps = sum(y)
+    avg_snps = total_snps/len(y)
+    print("Avg snps:", avg_snps)
 
-    counter = counter % 2
 
-#idx = [i for (i,v) in enumerate(y) if v >= 2]
-#x = [v for (i,v) in enumerate(x) if i in idx]
-#y = [v for (i,v) in enumerate(y) if i in idx]
-
-print("For k=100")
-total_paths = sum(x)
-avg_paths = total_paths/len(x)
-print("Avg paths:", avg_paths)
-total_snps = sum(y)
-avg_snps = total_snps/len(y)
-print("Avg snps:", avg_snps)
-
-f = open("output.k1000.20.txt")
-x = []
-y = []
-counter = 0
-for line in f.readlines():
-    if counter == 0:
-        x.append(len(line.split()))
-        counter += 1
-    else:
-        if line != "No SNPS\n":
-            y.append(len(line.split()))
-        else:
-            print("no SNPS")
-            y.append(0)
-        counter += 1
-    counter = counter % 2
-#idx = [i for (i,v) in enumerate(y) if v >= 2]
-#x = [v for (i,v) in enumerate(x) if i in idx]
-#y = [v for (i,v) in enumerate(y) if i in idx]
-print()
-print("For k=1000")
-total_paths = sum(x)
-avg_paths = total_paths/len(x)
-print("Avg paths:", avg_paths)
-total_snps = sum(y)
-avg_snps = total_snps/len(y)
-print("Avg snps:", avg_snps)
+if __name__ == "__main__":
+    for filename in [
+            "output.k1000.20.txt",
+            "output.k100.50.txt",
+            "output.k25.20.txt"
+    ]:
+        compute_stats(filename)
