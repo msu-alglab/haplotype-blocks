f8281e5cfacf2bc00bb5b0a9d2a504f41246eaad
Lucy <lgw2@uw.edu>
2020-01-14 15:20:58 -0700

    update scatter

--- pangenome/create_scatter.py
+++ pangenome/create_scatter.py
@@ -18,26 +18,7 @@ for line in f.readlines():
 
     counter = counter % 2
 
-plt.scatter(x,y, c="red", alpha=0.1, label="k=100")
-
-f = open("output.k1000.20.txt")
-x = []
-y = []
-counter = 0
-for line in f.readlines():
-    if counter == 0:
-        x.append(len(line.split()))
-        counter += 1
-    else:
-        if line != "No SNPS\n":
-            y.append(len(line.split()))
-        else:
-            print("no SNPS")
-            y.append(0)
-        counter += 1
-    counter = counter % 2
-
-plt.scatter(x,y, c="blue", alpha=0.3, label="k=1000")
+plt.scatter(x, y, c="red", alpha=0.1, label="k=100")
 
 f = open("output.k25.20.txt")
 x = []
@@ -56,7 +37,26 @@ for line in f.readlines():
         counter += 1
     counter = counter % 2
 
-plt.scatter(x,y, c="orange", alpha=0.3, label="k=25")
+plt.scatter(x, y, c="orange", alpha=0.3, label="k=25")
+
+f = open("output.k1000.20.txt")
+x = []
+y = []
+counter = 0
+for line in f.readlines():
+    if counter == 0:
+        x.append(len(line.split()))
+        counter += 1
+    else:
+        if line != "No SNPS\n":
+            y.append(len(line.split()))
+        else:
+            print("no SNPS")
+            y.append(0)
+        counter += 1
+    counter = counter % 2
+
+plt.scatter(x, y, c="blue", alpha=0.3, label="k=1000", s=10)
 
 plt.xlabel("Number of paths")
 plt.ylabel("Number of SNPs")
