80d01c65b3f0d8f5070973ec07b9a11e4285c261
Lucy <lgw2@uw.edu>
2020-01-14 15:14:01 -0700

    update scatter to include k=25

--- pangenome/create_scatter.py
+++ pangenome/create_scatter.py
@@ -39,6 +39,25 @@ for line in f.readlines():
 
 plt.scatter(x,y, c="blue", alpha=0.3, label="k=1000")
 
+f = open("output.k25.20.txt")
+x = []
+y = []
+counter = 0
+for line in f.readlines():
+    if counter == 0:
+        x.append(len(line.split()))
+        counter += 1
+    else:
+        if line != "No SNPS\n":
+            y.append(len(line.split()))
+        else:
+            print("no SNPS")
+            y.append(0)
+        counter += 1
+    counter = counter % 2
+
+plt.scatter(x,y, c="orange", alpha=0.3, label="k=25")
+
 plt.xlabel("Number of paths")
 plt.ylabel("Number of SNPs")
 
