050733c8af3c778088a48ccb3605c458c65b7849
Lucy <lgw2@uw.edu>
2020-01-15 14:13:04 -0700

    don't include duplicate paths in blocks

--- pangenome/create_scatter.py
+++ pangenome/create_scatter.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-f = open("output.k100.50.txt")
+f = open("output.k100.19.txt")
 x = []
 y = []
 counter = 0
@@ -20,7 +20,7 @@ for line in f.readlines():
 
 plt.scatter(x, y, c="red", alpha=0.1, label="k=100")
 
-f = open("output.k25.20.txt")
+f = open("output.k500.19.txt")
 x = []
 y = []
 counter = 0
@@ -37,29 +37,10 @@ for line in f.readlines():
         counter += 1
     counter = counter % 2
 
-plt.scatter(x, y, c="orange", alpha=0.3, label="k=25")
-
-f = open("output.k1000.20.txt")
-x = []
-y = []
-counter = 0
-for line in f.readlines():
-    if counter == 0:
-        x.append(len(line.split()))
-        counter += 1
-    else:
-        if line != "No SNPS\n":
-            y.append(len(line.split()))
-        else:
-            print("no SNPS")
-            y.append(0)
-        counter += 1
-    counter = counter % 2
-
-plt.scatter(x, y, c="blue", alpha=0.3, label="k=1000", s=10)
+plt.scatter(x, y, c="blue", alpha=0.3, label="k=500")
 
 plt.xlabel("Number of paths")
 plt.ylabel("Number of SNPs")
 
 plt.legend()
-plt.savefig("scatterplot.png")
+plt.savefig("scatterplot.pdf")
