c499e75ebccb21ab6784cf9f049d818f9ab258a4
Lucy <lgw2@uw.edu>
2020-01-16 10:12:49 -0700

    Make three scatterplots

--- wildcard/create_scatter.py
+++ wildcard/create_scatter.py
@@ -1,16 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-files = ["/home/bmumey/wh/chr22-full/output.txt.dist-0.0-500000.txt"]
+files = ["/home/bmumey/wh/chr22-full/output.txt.dist-0.0-500000.txt",
+         "/home/bmumey/wh/chr22-full/output.txt.dist-0.05-500000.txt",
+         "/home/bmumey/wh/chr22-full/output.txt.dist-0.1-500000.txt"]
+colors = ["red", "orange", "blue"]
 
-for file in files:
+fig, axes = plt.subplots(1, 3)
+fig.set_size_inches(10, 4)
+
+ax_index = 0
+
+for file, color in zip(files, colors):
     my_data = np.genfromtxt(file, delimiter=",")
     sampled = my_data[np.random.choice(my_data.shape[0], 10000, replace=False)]
     x = sampled[:, 0]
     y = sampled[:, 1]
-    plt.scatter(x, y, color="red", label="500000")
-    plt.xlabel("Number of paths")
-    plt.ylabel("Number of SNPs")
+    prop = file.split("-")[2]
+    ax = axes[ax_index]
+    ax.scatter(x, y, color=color, alpha=0.05)
+    ax.set_title(prop + " Proportion Wildcard")
+    ax_index += 1
+    if ax_index == 1:
+        ax.set(ylabel="Number of paths")
+    ax.set(xlabel="Number of SNPs")
 
-    plt.legend()
-    plt.savefig("scatterplot.pdf")
+plt.savefig("three_scatterplots.pdf")
