a3feef7fd23fdd735a583cd752ce938a31956998
Lucy <lgw2@uw.edu>
2020-01-16 11:47:36 -0700

    cleanup single scatterplot

--- wildcard/create_1_scatter.py
+++ wildcard/create_1_scatter.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 files = ["/home/bmumey/wh/chr22-full/output.txt.dist-0.1-500000.txt",
-         "/home/bmumey/wh/chr22-full/output.txt.dist-0.05-500000.txt",
+         # "/home/bmumey/wh/chr22-full/output.txt.dist-0.05-500000.txt",
          "/home/bmumey/wh/chr22-full/output.txt.dist-0.0-500000.txt"]
-colors = ["red",
-          "orange",
-          "blue"]
+colors = ["blue",
+          # "orange",
+          "red"]
 
 
 for file, color in zip(files, colors):
@@ -15,7 +15,7 @@ for file, color in zip(files, colors):
     x = sampled[:, 0]
     y = sampled[:, 1]
     prop = file.split("-")[2]
-    plt.scatter(x, y, color=color, alpha=0.1, label=prop, s=5)
+    plt.scatter(x, y, color=color, alpha=0.05, label=prop, s=10)
 
 plt.xlabel("Number of paths")
 plt.ylabel("Number of SNPs")
